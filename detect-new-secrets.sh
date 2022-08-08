bf855b904a8bca17a334986797650dacec7ed529
Ben Golding <60831565+bgolding355@users.noreply.github.com>
2022-08-08 13:59:46 -0400

    Fix no warn (#21)
    * Fixing flag dropping
    
    Previously I committed 4f6ffe9348def3380fdd97610c6ac571f0c37b47. Here, I
    introduced a bug that caused files sometimes not to be read.
    
    Fixing in this commit
    
    * Logging arguments to detect-secrets
    
    There is quite a bit of logic that goes into constructing these
    arguments. By logging them, I will make it easier to detect problems in
    the future.

--- detect-new-secrets.sh
+++ detect-new-secrets.sh
@@ -8,21 +8,22 @@ fetch_flags_from_file() {
     file_to_check="$2"
     
     flags=()
-    (while read line; do 
+    while read line; do 
         if [[ "${line::1}" != '#' ]] && [[ ! -z "$line" ]]; then
             flag="$flag_to_add $line "
             flags+="$flag"
         fi
-    done < "$file_to_check") 2>&1 > /dev/null
+    done < "$file_to_check"
 
     echo "$flags"
 }
 
 scan_new_secrets() {
-    excluded_files=$(fetch_flags_from_file '--exclude-files' "$EXCLUDE_FILES_PATH")
-    excluded_secrets=$(fetch_flags_from_file '--exclude-secrets' "$EXCLUDE_SECRETS_PATH")
-    excluded_lines=$(fetch_flags_from_file '--exclude-lines' "$EXCLUDE_LINES_PATH")
+    excluded_files=$(fetch_flags_from_file '--exclude-files' "$EXCLUDE_FILES_PATH" 2>/dev/null)
+    excluded_secrets=$(fetch_flags_from_file '--exclude-secrets' "$EXCLUDE_SECRETS_PATH" 2>/dev/null)
+    excluded_lines=$(fetch_flags_from_file '--exclude-lines' "$EXCLUDE_LINES_PATH" 2>/dev/null)
     detect_secret_args="$excluded_files $excluded_secrets $excluded_lines $DETECT_SECRET_ADDITIONAL_ARGS"
+    echo "Running detect-secrets with args: $detect_secret_args"
 
     detect-secrets scan $detect_secret_args --baseline "$BASELINE_FILE"
     detect-secrets audit "$BASELINE_FILE" --report --json > "$all_secrets_file"
